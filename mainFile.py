9fd4fdeb8d3abb8deaabd106ef167e09c4ca9a34
TMmarius <marius.tataru@5psolutions.eu>
2020-01-16 10:50:38 +0200

    add some ai to code

--- mainFile.py
+++ mainFile.py
@@ -1,41 +1,62 @@
 import os
 import xml.etree.ElementTree as etree
+from collections import defaultdict
+
+a_store = defaultdict(list)
+b_store = defaultdict(list)
+c_store = defaultdict(list)
+d_store = defaultdict(list)
 
 
 # Toate documentele care conțin elementul(tag-ul) “document”
 def a(collection_path, tag_name):
     answer = []
-    found = False
+    if len(a_store[tag_name]):
+        return a_store[tag_name]
+
     for file_name in os.listdir(collection_path):
         root = etree.parse(collection_path + "/" + file_name).getroot()
         element = root.find(".//" + tag_name)
         if element is None:
             continue
         answer.append(file_name)
+        a_store[tag_name].append(file_name)
+
+        # add file to global store, so the next search will take data from store
+    a_store[tag_name].append("rwturnwd from a_store")
     return answer
 
 
 # Toate documentele care conțin cuvântul “proiect” sub elementul “word”.
 def b(collection_path, tag_name, tag_content):
     answer = []
-    found = False
+
+    if len(b_store[tag_name + "_" + tag_content]):
+        return b_store[tag_name + "_" + tag_content]
+
     for file_name in os.listdir(collection_path):
         root = etree.parse(collection_path + "/" + file_name).getroot()
         for element in root.findall(".//" + tag_name):
             if element.text == tag_content:
                 answer.append(file_name)
+                b_store[tag_name + "_" + tag_content].append(file_name)
                 continue
+    b_store[tag_name + "_" + tag_content].append("this data is from store")
     return answer
 
 
 # Toate documentele care conțin un anumit număr de elemente.
 def c(collection_path, elem_count):
     answer = []
-    found = False
+
+    if len(c_store[elem_count]):
+        return c_store[elem_count]
     for file_name in os.listdir(collection_path):
         root = etree.parse(collection_path + "/" + file_name).getroot()
         if len(root.findall(".//")) >= elem_count:
             answer.append(file_name)
+            c_store[elem_count].append(file_name)
+    c_store[elem_count].append("this data is from store")
     return answer
 
 
@@ -54,12 +75,16 @@ def node_ident(node, ident):
 # Toate documentele care au o adâncime a arborelui XML mai mare de 5.
 def d(collection_path, depth):
     answer = []
-    found = False
+
+    if len(d_store[depth]):
+        return d_store[depth]
+
     for file_name in os.listdir(collection_path):
         root = etree.parse(collection_path + "/" + file_name).getroot()
         if node_ident(root, depth):
             answer.append(file_name)
-            found = True
+            d_store[depth].append(file_name)
+    d_store[depth].append("this data is from store")
     return answer
 #
 # a('xmlCollection', "document")
